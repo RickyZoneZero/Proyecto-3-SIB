f99baccac6c340162387af8d23886a74045509c7
mike1816 <mike.lp560@gmail.com>
2022-05-27 13:28:25 -0500

    lista

--- ransomware.py
+++ ransomware.py
@@ -1,7 +1,13 @@
 import rescueWindow
+import pathlib
 #Programa que contiene la función principal para ejecutar el proyecto.
 
 def main():
     rescueWindow.rescueWindow() 
 
 main()
+
+ejemplo_dir = 'C:/' #Muestra una lista de archivos que contiene la unidad C
+directorio = pathlib.Path(ejemplo_dir)
+for fichero in directorio.iterdir():
+    print(fichero.name)

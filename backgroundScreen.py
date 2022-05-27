7d86a56342b426905e70ee7a84fc221989a42010
Enrique <java.zone.in.out@gmail.com>
2022-05-26 19:31:08 -0500

    Cambiando el path de relativo a absoluto

--- backgroundScreen.py
+++ backgroundScreen.py
@@ -6,4 +6,4 @@ import ctypes
 #Parámetro de interfaz
 SPI_SETDESKWALLPAPER = 20 
 #Interfaz directa de Windows para cambiar la imagen de fondo.
-ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, "/images/prueba.jpg" , 0)
+ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, r"C:\Users\enriq\Desktop\Proyecto_3_SIB\images\prueba.jpg" , 0)

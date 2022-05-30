8644c81281ca723dacb79570945852101fb67a8b
Enrique <java.zone.in.out@gmail.com>
2022-05-30 09:45:39 -0500

    Removiendo módulo que no se utilizó

--- fileList.py
+++
@@ -1,9 +0,0 @@
-#Programa que lista los directorios contenidos en C:\
-
-#Librería de rutas de sistemas orientadas a objetos.
-import pathlib
-
-ejemplo_dir = 'C:/' #Muestra una lista de archivos que contiene la unidad C
-directorio = pathlib.Path(ejemplo_dir)
-for fichero in directorio.iterdir():
-    print(fichero.name)
